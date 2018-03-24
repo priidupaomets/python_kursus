15eaccd86e9647c21e5a7f3e9d66a0bc9074ee27
Priidu Paomets <priidu.paomets@gmail.com>
2018-03-24 10:06:43 +0200

    Täiendatud veahaldus

--- Overview/12 - veahaldus.py
+++ Overview/12 - veahaldus.py
@@ -47,4 +47,36 @@ print("-"*30)
 print(numavg())             
 
 # Vigade välja kutsumine
+raise ValueError('Esimese argumendi väärtus peab olema vahemikus 1..50')
 raise IOError('file error')
+
+
+#
+# with-avaldis 
+#
+
+# Tavaline kaitsmata vorm faili kasutusest, ressursid vabastatakse kunagi hiljem
+for rida in open('minufail.txt'):
+    print(rida, end = "")
+
+# Natuke parem vorm, kus me teadlikult sulgeme faili 
+f = open('minufail.txt')
+
+for rida in f:
+    print(rida, end = "")
+
+f.close()
+
+# try-finally poolt kaitstud vorm, kuid ei kata faili avamisel tekkivaid vigu
+f = open('minufail.txt')
+try:
+    for rida in f:
+        print(rida, end = "")
+finally:
+    f.close()
+
+
+# Kaitstud vorm ja lisaks vabastab ressursid kohe kui lõpetab
+with open('minufail.txt') as f:
+    for rida in f:
+        print(rida, end = "")
